5cd31d5c0f6fe9386284f77025150fe168cc9d0e
Nikita <nikitapohod@icloud.com>
2023-08-25 17:56:29 +0300

    Added basic functionality for the bot, added parsing of coordinates by pressing a button

---
+++ main.py
@@ -0,0 +1,32 @@
+import logging
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+
+# Working with .env file and env variables
+load_dotenv("env/config.env")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=['location'])
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    text = "latitude:  {}\nlongitude: {}".format(lat, lon)
+    await message.answer(text)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_help(message: types.Message):
+    text = f"Hello, <b>{message.from_user.full_name}</b>! \nClick on the button below to share your location!"
+    await message.answer(text, reply_markup=get_keyboard())
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
